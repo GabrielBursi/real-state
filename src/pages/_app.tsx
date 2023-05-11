8dc68e4fd3f8868fe7d03f3f1469a7bc394b4f5f
Gabriel Bursi <gabriellbursii@gmail.com>
2023-05-10 23:12:39 -0300

    deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,27 @@
+import { Router } from 'next/router';
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
-import NextNProgress from 'nextjs-progressbar';
+import NProgress from 'nprogress';
 import Layout from '@/layout'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  });
+
   return (
     <>
       <Head>
-          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       </Head>
-      <NextNProgress color="#4d99e1" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
         <ChakraProvider>
           <Layout>
             <Component {...pageProps} />
